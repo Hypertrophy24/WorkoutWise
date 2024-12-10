3f5ea569253864204bad21f58c5a1a46f5c9e71f
Hypertrophy24 <96507138+Hypertrophy24@users.noreply.github.com>
2024-12-09 20:12:17 -0700

    Top bar branding added

--- app/src/main/java/com/example/workoutwise/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/workoutwise/ui/theme/MainScreen.kt
@@ -2,7 +2,12 @@
 package com.example.workoutwise.ui.theme
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
@@ -10,11 +15,21 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.workoutwise.navigation.Screen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(mainViewModel: MainViewModel) {
     val navController = rememberNavController()
 
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("WorkoutWise") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        },
         bottomBar = {
             BottomNavigationBar(navController = navController)
         }
